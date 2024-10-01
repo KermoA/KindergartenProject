acbf91cfda5bafcd0c808838cd4c18d93d7f33e1
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-10-02 01:13:04 +0300

    KindergartenCreate

--- KindergartenProject/KindergartenProject.ApplicationServices/Services/KindergartensServices.cs
+++ KindergartenProject/KindergartenProject.ApplicationServices/Services/KindergartensServices.cs
@@ -18,6 +18,24 @@ namespace KindergartenProject.ApplicationServices.Services
 			_context = context;
 		}
 
+		public async Task<Kindergarten> Create(KindergartenDto dto)
+		{
+			Kindergarten kindergarten = new Kindergarten();
+
+			kindergarten.Id = Guid.NewGuid();
+			kindergarten.GroupName = dto.GroupName;
+			kindergarten.ChildrenCount = dto.ChildrenCount;
+			kindergarten.KindergartenName = dto.KindergartenName;
+			kindergarten.Teacher = dto.Teacher;
+			kindergarten.CreatedAt = DateTime.Now;
+			kindergarten.UpdatedAt = DateTime.Now;
+
+			await _context.Kindergartens.AddAsync(kindergarten);
+			await _context.SaveChangesAsync();
+
+			return kindergarten;
+		}
+
 		public async Task<Kindergarten> DetailAsync(Guid id)
 		{
 			var result = await _context.Kindergartens
